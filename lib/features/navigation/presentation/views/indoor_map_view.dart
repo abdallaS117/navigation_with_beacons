684687a97b77f1656a4e3322bf13a0fd5e7ee1b5
Samir <141153191+abdallaMoyasar@users.noreply.github.com>
2026-03-08 14:43:54 +0200

    packaging 3

--- lib/features/navigation/presentation/views/indoor_map_view.dart
+++ lib/features/navigation/presentation/views/indoor_map_view.dart
@@ -120,19 +120,9 @@ class _IndoorMapViewState extends State<IndoorMapView>
     await Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (_) => Theme(
-          data: ThemeData.light().copyWith(
-            scaffoldBackgroundColor: Colors.grey[100],
-            appBarTheme: AppBarTheme(
-              backgroundColor: Colors.blue,
-              foregroundColor: Colors.white,
-              elevation: 2,
-            ),
-          ),
-          child: BlocProvider.value(
-            value: context.read<ConfigurationCubit>(),
-            child: const ConfigurationHomeScreen(),
-          ),
+        builder: (_) => BlocProvider.value(
+          value: context.read<ConfigurationCubit>(),
+          child: const ConfigurationHomeScreen(),
         ),
       ),
     );
